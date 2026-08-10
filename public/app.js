533c5bd3718993cd5803370438be42171d6d6af8
Prateek Jain <prateek.rj.jain@gmail.com>
2026-08-10 15:02:30 +0530

    Change Leaflet map tile layer to light mode

--- public/app.js
+++ public/app.js
@@ -1870,7 +1870,7 @@
             scrollWheelZoom: true
         }).setView([22.5, 78.9], 5);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
             subdomains: 'abcd',
             maxZoom: 19
