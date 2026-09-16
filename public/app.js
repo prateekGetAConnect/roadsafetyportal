b1f2e456d702a3062ac4b6053c6468268d83d3f5
Prateek Jain <prateek.rj.jain@gmail.com>
2026-09-16 11:45:59 +0530

    Fix Map API key error by switching from CartoDB to OSM tiles

--- public/app.js
+++ public/app.js
@@ -1989,10 +1989,10 @@
             scrollWheelZoom: true
         }).setView([22.5, 78.9], 5);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 19
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
+            maxZoom: 19,
+            className: 'dark-tiles'
         }).addTo(state.map);
 
         state.markerClusterGroup = L.markerClusterGroup({
